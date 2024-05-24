6c444f2607209d9690f17b5d4bd735ff7c792606
Marco Finschi <Marco.Finschi@lu.ch>
2024-05-24 08:52:13 +0200

    Starter MAUI .NET 8 project

---
+++ MauiRadioButtonTextColor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRadioButtonTextColor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
